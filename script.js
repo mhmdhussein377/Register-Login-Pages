fe1e1640712b66dee6ec3bb139acd15eec95ba00
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-20 10:23:56 +0300

    connected the signin page with the backend

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 document
-    .getElementById('myForm')
+    .getElementById('signin-form')
     .addEventListener('submit', function (event) {
-        event.preventDefault();
+        event.preventDefault()
+
+        const username = document
+            .querySelector(".signin-form .username")
+            .value;
+        const password = document
+            .querySelector(".signin-form .password")
+            .value;
+
+        let signinData = new URLSearchParams();
+        signinData.append("username", username);
+        signinData.append("password", password);
+
+        fetch("http://localhost/signuplogin/signup.php", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: signinData
+        }).then((respnse) => respnse.json()).then((data) => {
+            console.log("Response from server: ", data);
+        }).catch((error) => {
+            console.log(error);
+        });
     });
